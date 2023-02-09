79a5459cbc60c11a1770c87ca5e02b6bcfd2c903
deger4beger <deger.begerrr@gmail.com>
2023-02-09 23:59:41 +0600

    init: list component

---
+++ src/components/records-list.tsx
@@ -0,0 +1,28 @@
+import { useAppSelector } from "../redux";
+
+const RecordsList = ({
+  selected,
+  setSelected,
+}: {
+  selected: string;
+  setSelected: (selected: string) => void;
+}) => {
+
+  const records = useAppSelector((state) => state.recordReducer.data);
+
+  return (
+    <ul className="records">
+      {records.map((record) => (
+        <li
+          key={record.id}
+          className={"record" + (record.id === selected ? " selected" : "")}
+          onClick={() => setSelected(record.id)}
+        >
+          {record.text}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default RecordsList;
