3a2d89d86f6e5baf16810fe7aa99c6c4697f9a8e
deger4beger <deger.begerrr@gmail.com>
2023-02-09 23:15:09 +0600

    feat: staged

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,50 @@
-import "./App.css"
-import { useAppDispatch, useAppSelector } from "./redux"
+import { useState } from "react";
+import "./App.css";
+import { useAppDispatch, useAppSelector } from "./redux";
 import { createRecord } from "./redux/record";
 
 function App() {
-
+  const [selected, setSelected] = useState("");
+  const records = useAppSelector((state) => state.recordReducer.data);
   const dispatch = useAppDispatch();
-  const records = useAppSelector(state => state.recordReducer.data);
 
   const onCreateRecord = () => {
     const record = window.prompt("Введите текст записи");
-    if (!record) return
-    dispatch(createRecord({
-      text: record
-    }))
-  }
+    if (!record) return;
+    dispatch(
+      createRecord({
+        text: record,
+      })
+    );
+  };
+
+  const onSetSelected = (id: string) =>
+    selected === id ? setSelected("") : setSelected(id);
 
   // const onDeleteRecord
 
   return (
     <div className="wrapper">
       <div className="buttons">
-        <button className="button">
+        <button className="button" onClick={onCreateRecord}>
           Добавить
         </button>
-        <button className="button">
-          Удалить
-        </button>
-        <button className="button">
-          Тест GraphQL
-        </button>
+        <button className="button">Удалить</button>
+        <button className="button">Тест GraphQL</button>
       </div>
       <div className="records">
-        {
-          records.map(record =>
-            <div className="record">
-              { record.text }
-            </div>
-          )
-        }
+        {records.map((record) => (
+          <div
+            key={record.id}
+            className={"record" + (record.id === selected ? " selected" : "")}
+            onClick={() => onSetSelected(record.id)}
+          >
+            {record.text}
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { setupStore } from './redux'
+import { Provider } from "react-redux"
+
+const store = setupStore()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
